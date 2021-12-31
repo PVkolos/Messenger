e1d9ddd49d6e5bdcd2fea6426754614c569dca17
PVkolos <70021890+PVkolos@users.noreply.github.com>
2021-12-31 17:57:36 +0300

    Update Chats.py

--- Chats.py
+++ Chats.py
@@ -1,9 +1,10 @@
 import requests
 from datetime import datetime
+from PIL import Image
 
 from PyQt5 import uic, QtCore
 from PyQt5.QtCore import QSize
-from PyQt5.QtGui import QIcon
+from PyQt5.QtGui import QIcon, QPixmap
 from PyQt5.QtWidgets import QMainWindow
 import main
 from SETTINGS import URL
@@ -17,22 +18,40 @@ class Chat(QMainWindow):
         self.after = 0
         f = open('Разное/name.txt', 'r', encoding='utf8')
         self.nm = nm
-        self.name = f.readline()
+        self.id = f.readline()
         f.close()
-        self.label.setText(self.name)
+        r = requests.get(f'{URL}users')
+        for el in r.json()['users']:
+            if str(el[2]) == self.id:
+                self.label.setText(el[0])
         self.pushButton_2.clicked.connect(self.send)
         self.timer = QtCore.QTimer()
         self.timer.timeout.connect(self.get_mess)
         self.timer.start(1000)
         self.pushButton_2.setIcon(QIcon('Разное/Снимок.PNG'))
         self.pushButton_2.setIconSize(QSize(300, 300))
+        try:
+            try:
+                r = requests.get(f'{URL}users')
+            except Exception:
+                return
+            for el in r.json()['users']:
+                if str(el[2]) == self.id:
+                    img = Image.open(el[-1])
+                    img = img.resize((80, 50), Image.ANTIALIAS)
+                    img.save('Разное/ava2.png')
+                    self.pixmap = QPixmap('Разное/ava2.png')
+                    self.label_3.setPixmap(self.pixmap)
+                    self.pushButton.clicked.connect(self.image)
+        except Exception:
+            pass
 
     def send(self):
         """  отправка сообщений на сервер  """
         self.messege = self.textEdit_2.toPlainText()
         try:
             r = requests.post(f'{URL}send_ch',
-                              json={'otpr': main.name2, 'messege': self.messege, 'name': self.nm})
+                              json={'otpr': main.id2, 'messege': self.messege, 'name': self.nm})
         except Exception as e:
             print(e, 1)
             self.textBrowser.append('Ошибка сервера, попробуйте позднее!')
@@ -47,6 +66,7 @@ class Chat(QMainWindow):
 
     def get_mess(self):
         """  поиск новых сообщений на сервере  """
+        print(1)
         try:
             r = requests.get(f'{URL}get_messages?after={self.after}',
                              json={'name': self.nm})
@@ -67,7 +87,7 @@ class Chat(QMainWindow):
         try:
             for el in messages:
                 time = str(datetime.fromtimestamp(el[-1])).split()[1].split(':')
-                if el[0] == main.name2:
+                if str(el[0]) == main.id2:
                     j = 40
                     self.textBrowser.append('\t\t\t\t\t\t' + time[0] + ':' + time[1])
                     for i in range(0, len(el[1]), 40):
