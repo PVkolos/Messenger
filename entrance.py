3224a02900c5b544be7635f430f1920e614fef03
PVkolos <70021890+PVkolos@users.noreply.github.com>
2021-12-31 17:56:43 +0300

    Update entrance.py

--- entrance.py
+++ entrance.py
@@ -1,3 +1,4 @@
+import time
 from hashlib import sha256
 import requests
 
@@ -20,7 +21,7 @@ class Avtoriz(QMainWindow):
         """  регистрация пользователя в мессенджере  """
         try:
             r = requests.post(f'{URL}add_user',
-                              json={'name': self.lineEdit_3.text(), 'password': self.lineEdit_4.text()})
+                              json={'name': self.lineEdit_3.text(), 'password': self.lineEdit_4.text(), 'path': 'Разное/ava.png'})
         except Exception:
             self.label_3.setText('Ошибка сервера, попробуйте позднее!')
             self.label_3.setStyleSheet("QLabel { background-color: rgba(255, 0, 0, 140); }")
@@ -33,12 +34,19 @@ class Avtoriz(QMainWindow):
             print(f'Статус код сервера: {r.status_code}')
             return
         else:
-            f = open('Разное/name.txt', 'w')
-            f.write(self.lineEdit_3.text())
-            f.close()
-            self.second_form = main.Main()
-            self.second_form.show()
-            self.hide()
+            try:
+                g = requests.get(f'{URL}users')
+                for el in g.json()['users']:
+                    if self.lineEdit_3.text() == el[0]:
+                        f = open('Разное/name.txt', 'w')
+                        f.write(str(el[2]))
+                        f.close()
+                        self.second_form = main.Main()
+                        self.second_form.show()
+                        self.hide()
+            except Exception as e:
+                print(e)
+                return
 
     def avt(self):
         """  вход в аккаунт, проверка данных на валидность и верность  """
@@ -48,15 +56,18 @@ class Avtoriz(QMainWindow):
             self.label_4.setText('Ошибка сервера, попробуйте позднее!')
             self.label_4.setStyleSheet("QLabel { background-color: rgba(255, 0, 0, 140); }")
             return
-        for el in r.json()['users']:
-            password = sha256(self.lineEdit_2.text().encode('utf-8')).hexdigest()
-            if el[0] in self.lineEdit.text() and el[1] == password:
-                f = open('Разное/name.txt', 'w')
-                f.write(self.lineEdit.text())
-                f.close()
-                self.second_form = main.Main()
-                self.second_form.show()
-                self.hide()
-                return
-        self.label_4.setText('Неверный логин или пароль!')
-        self.label_4.setStyleSheet("QLabel { background-color: rgba(255, 0, 0, 140); }")
+        try:
+            for el in r.json()['users']:
+                password = sha256(self.lineEdit_2.text().encode('utf-8')).hexdigest()
+                if el[0] == self.lineEdit.text() and el[1] == password:
+                    f = open('Разное/name.txt', 'w')
+                    f.write(str(el[2]))
+                    f.close()
+                    self.second_form = main.Main()
+                    self.second_form.show()
+                    self.hide()
+                    return
+            self.label_4.setText('Неверный логин или пароль!')
+            self.label_4.setStyleSheet("QLabel { background-color: rgba(255, 0, 0, 140); }")
+        except Exception as e:
+            print(e)
